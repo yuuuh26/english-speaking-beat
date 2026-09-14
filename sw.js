c835c287e1e6d65f4bd183a0867b3f84d1e14a3f
yuuuh26 <dengana.10011212@gmail.com>
2026-09-15 01:36:56 +0900

    Use maskable PWA icon to remove Android white padding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "speak-beat-v1.0.3";
+const CACHE = "speak-beat-v1.0.4";
 const CORE = [
   "./", "./index.html", "./css/app.css", "./manifest.webmanifest",
   "./js/app.js", "./js/game.js", "./js/judge.js", "./js/scoring.js", "./js/speech.js", "./js/audio.js", "./js/storage.js", "./js/backup.js",
